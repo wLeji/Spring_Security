9dd1a6f3bff670b41ecb26e848029fb3e40ea9b1
Simon Bédard <JavaKhanStudio@Gmail.com>
2025-02-17 17:24:15 +0100

    Update User.java

--- src/main/java/com/security/entity/User.java
+++ src/main/java/com/security/entity/User.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true, nullable = false)
     private String username;
